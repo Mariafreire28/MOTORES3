b80833929c77ada537695c05ff799e83882d29e2
Mariafreire28 <g.gracas@escolar.ifrn.edu.br>
2025-10-20 17:38:57 -0300

    Among Us

---
+++ AVALIAÇAOETAPA3/Assets/Scripts/Dano.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Dano : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);          
+        }
+    }
+}
